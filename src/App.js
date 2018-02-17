06a9c68e451962dc2c8612224e13cd3455a0ab27
thegrims <grimshawaidan@gmail.com>
2018-02-17 00:07:32 -0800

    pushed important stuff

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">WelReact</h1>
         </header>
         <p className="App-intro">
+        <button bsStyle="primary">Primary</button>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
